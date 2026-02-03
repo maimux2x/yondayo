416231608ba584e83e2f4589041d95b90d277470
maimux2x <m0smile10often@gmail.com>
2026-02-03 21:06:56 +0900

    Validate feature date on published_at

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,14 @@ class Book < ApplicationRecord
 
   validates :title,  presence: true
   validates :author, presence: true
+
+  validate :published_at_cannot_be_feature
+
+  def published_at_cannot_be_feature
+    return unless published_at.present?
+
+    if published_at > Date.today
+      errors.add(:published_at, '未来の日付は使えません。')
+    end
+  end
 end
