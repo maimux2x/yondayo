33176795a2ff0cad463677f919e397e340f97ec0
maimux2x <m0smile10often@gmail.com>
2026-02-15 20:33:14 +0900

    Improve reading logs index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def status_badge_class(status)
+    case status
+    when "read"     then "success"
+    when "progress" then "primary"
+    when "unread"   then "secondary"
+    end
+  end
 end
