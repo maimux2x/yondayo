300914a787738b63f95ee9aaf5563d8393ddf364
maimux2x <m0smile10often@gmail.com>
2026-03-29 21:24:04 +0900

    Search with Google Books API

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,20 @@
 class BooksController < ApplicationController
-  def create
-    isbn      = params.expect(:isbn)
-    res       = HTTPX.get('https://api.openbd.jp/v1/get', params: {isbn:})
-    payload   = res.json(symbolize_names: true).first
-    title     = payload.dig(:summary, :title)
-    author    = payload.dig(:summary, :author)
-    cover_url = payload.dig(:summary, :cover)
+  def search
+    res = HTTPX.get('https://www.googleapis.com/books/v1/volumes', params: {
+      q:   params.expect(:q),
+      key: ENV['BOOKS_API_KEY']
+    })
 
-    book = Book.find_or_initialize_by(isbn:)
-    book.update!(
-      title:,
-      author:,
+    payloads = res.json(symbolize_names: true)
 
-      cover: cover_url.present? ? {
-        io:       URI.open(cover_url),
-        filename: File.basename(cover_url)
-      } : nil
-    )
+    @books = payloads[:items][0..2].map {|payload|
+      {
+        id:     payload[:id],
+        title:  payload.dig(:volumeInfo, :title),
+        author: payload.dig(:volumeInfo, :authors).join(', ')
+      }
+    }
 
-    if reading = Current.user.readings.find_by(book:)
-      redirect_to edit_reading_path(reading), status: :see_other
-    else
-      redirect_to new_reading_path(book_id: book.id), status: :see_other
-    end
+    render 'readings/new'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   root 'readings#index'
 
   resources :readings, only: %i[index new create show edit update destroy]
-  resources :books,    only: %i[create update]
+
+  resources :books, only: %i[create update] do
+    collection do
+      get 'search'
+    end
+  end
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
