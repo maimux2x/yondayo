a4181a8c01ba15736e7865feb2ca7177b97c86c8
maimux2x <m0smile10often@gmail.com>
2026-04-05 19:17:52 +0900

    Add OGP

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
-  has_one_attached :cover
+  has_one_attached :cover do |attachable|
+    attachable.variant :ogp, resize_to_pad: [1200, 630, {background: 'white'}]
+  end
 
   has_many :readings, dependent: :destroy
 
